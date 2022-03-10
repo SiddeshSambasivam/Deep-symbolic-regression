3593fc29f612d356ec15e7702747c1dd922f5180
Siddesh Sambasivam Suseela <siddesh.suseela@shopee.com>
2022-03-10 17:54:38 +0800

    add boilerplate

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import model as m
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/model.py
@@ -0,0 +1,7 @@
+import torch
+
+class SymbolicLayer:
+    """Represents a fully connected NN with custom activation functions"""
+
+class SymbolicNN:
+    """A NN which has SymbolicLayer as hidden layers"""
