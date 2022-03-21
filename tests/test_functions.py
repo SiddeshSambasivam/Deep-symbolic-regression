06626ff20c756c53e85a710062f9fd5e416715e5
Siddesh Sambasivam Suseela <siddesh.suseela@shopee.com>
2022-03-21 16:47:36 +0800

    add symbolic layer unit tests

---
+++ tests/test_functions.py
@@ -0,0 +1,20 @@
+import torch
+import unittest
+import src as P
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_square(self):
+        x = torch.randint(0, 3, torch.Size([3,1]))
+        sq = P.Square()
+        torch.testing.assert_allclose(torch.square(x), sq.torch(x))
+
+    def test_sigmoid(self):
+        x = torch.randint(0, 3, torch.Size([3,1]))
+        sig = P.Sigmoid()
+        torch.testing.assert_allclose(torch.sigmoid(x), sig.torch(x))
+        print(sig.name)
+
+
+        
---
+++ tests/test_symbolic_network.py
@@ -0,0 +1,18 @@
+import unittest
+import src as P
+import torch
+
+class TestSymbolicNN(unittest.TestCase):
+
+    def test_output_dimension(self):
+        """Test to check the dimension of the output of a symbolic layer"""
+        dim = (100, 4)
+        input_dim = dim[1]
+
+        n_funcs = len(P.default_funcs)
+        x = torch.randn(dim)
+
+        sym_layer_1 = P.SymbolicLayer(input_size=input_dim, funcs=P.default_funcs)
+        out = sym_layer_1(x)
+
+        assert out.shape == (100, n_funcs)
